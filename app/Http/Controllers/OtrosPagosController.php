0c4bc4d1c06e4ac4383543cc0b743e4f05b9ced4
Ivan <ivan.saez@informatica.isbast.com>
2021-01-18 10:00:45 -0300

    idBoleta nulo

--- app/Http/Controllers/OtrosPagosController.php
+++ app/Http/Controllers/OtrosPagosController.php
@@ -18,43 +18,48 @@ class OtrosPagosController extends Controller
         $boleta = Boleta::where('idBoleta',$request->p_idcli)
             ->where('idEstado',2)
             ->first();
+        if($boleta){
 
-        $convenio = getenv('OTROS_PAGOS_COVENIO');
-        //Log::info($convenio);
-        $key = $request->p_fectr.$request->p_tid.$convenio;
-        $llave = str_pad($key,16);
-        $encriptacion = openssl_encrypt($llave, "AES-256-CBC",getenv('OTROS_PAGOS_KEY'),1,getenv('OTROS_PAGOS_IV'));
-        //Log::info($encriptacion);
-        $h_firma = base64_encode($encriptacion);
-        //Log::info($h_firma);
-        $headers = apache_request_headers();
-        foreach($headers as $header => $value){
-            if($header == "H-Firma"){
-                if($value != $h_firma){
-                    return response()->json(['r_retcod' => "65"],200);
+            $convenio = getenv('OTROS_PAGOS_COVENIO');
+            //Log::info($convenio);
+            $key = $request->p_fectr.$request->p_tid.$convenio;
+            $llave = str_pad($key,16);
+            $encriptacion = openssl_encrypt($llave, "AES-256-CBC",getenv('OTROS_PAGOS_KEY'),1,getenv('OTROS_PAGOS_IV'));
+            //Log::info($encriptacion);
+            $h_firma = base64_encode($encriptacion);
+            //Log::info($h_firma);
+            $headers = apache_request_headers();
+            foreach($headers as $header => $value){
+                if($header == "H-Firma"){
+                    if($value != $h_firma){
+                        return response()->json(['r_retcod' => "65"],200);
+                    }
                 }
             }
-        }
-        $idTransaccion = (int)$request->p_tid;
-        if($idTransaccion){
-            return response()->json([
-                'r_tid' => $idTransaccion,
-                'r_retcod' => "00",
-                'r_ndoc' => "0001",
-                'r_docs' => [
-                    array(
-                        'r_doc' => "".$boleta->idBoleta."",
-                        'r_mnt' => str_pad(strval($boleta->totalBoleta),10,"0",STR_PAD_LEFT)."00",
-                        'r_mnv' => str_pad(strval($boleta->totalBoleta),10,"0",STR_PAD_LEFT)."00",
-                        'r_fev' => strftime("%Y%m%d",strtotime($boleta->fechaVencimiento)),
-                        'r_fem' => date('Ymd'),
-                        'r_det' => $h_firma 
-                    )
-                ]
-            ], 200);
+            $idTransaccion = (int)$request->p_tid;
+            if($idTransaccion){
+                return response()->json([
+                    'r_tid' => $idTransaccion,
+                    'r_retcod' => "00",
+                    'r_ndoc' => "0001",
+                    'r_docs' => [
+                        array(
+                            'r_doc' => "".$boleta->idBoleta."",
+                            'r_mnt' => str_pad(strval($boleta->totalBoleta),10,"0",STR_PAD_LEFT)."00",
+                            'r_mnv' => str_pad(strval($boleta->totalBoleta),10,"0",STR_PAD_LEFT)."00",
+                            'r_fev' => strftime("%Y%m%d",strtotime($boleta->fechaVencimiento)),
+                            'r_fem' => date('Ymd'),
+                            'r_det' => $h_firma 
+                        )
+                    ]
+                ], 200);
+            }else{
+                return response()->json(['r_retcod'=>"03"],200);
+            }
         }else{
             return response()->json(['r_retcod'=>"03"],200);
         }
+
     }
     public function notpag01req(Request $request){
         $idTransaccion = (int)$request->p_tid;
@@ -80,6 +85,7 @@ class OtrosPagosController extends Controller
                 $boleta->save();
                 $numeros = Numero::where('idBoleta',$request->p_idcli)->get();
                 foreach($numeros as $numero){
+                    $numero->idBoleta = null;
                     $numero->idEstado = 3;
                     $numero->save();
                 }
