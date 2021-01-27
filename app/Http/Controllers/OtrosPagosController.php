35149018f46126e10df73e2205a352ece68740e4
Ivan <ivan.saez@informatica.isbast.com>
2021-01-27 15:14:39 -0300

    cambio de h-firma

--- app/Http/Controllers/OtrosPagosController.php
+++ app/Http/Controllers/OtrosPagosController.php
@@ -16,29 +16,28 @@ class OtrosPagosController extends Controller
     public function condeu01req(Request $request){
 
         //Log::info($request);
-        
+        $convenio = getenv('OTROS_PAGOS_COVENIO');
+        //Log::info($convenio);
+        $key = $request->p_fectr.$request->p_tid.$convenio;
+        $llave = str_pad($key,16);
+        $encriptacion = openssl_encrypt($llave, "AES-256-CBC",getenv('OTROS_PAGOS_KEY'),1,getenv('OTROS_PAGOS_IV'));
+        //Log::info($encriptacion);
+        $h_firma = base64_encode($encriptacion);
+        //Log::info($h_firma);
+        $headers = apache_request_headers();
+        foreach($headers as $header => $value){
+            if($header == "H-Firma"){
+                if($value != $h_firma){
+                    return response()->json(['r_retcod' => "65"],200);
+                }
+            }
+        }
+
         $boleta = Boleta::where('idBoleta',$request->p_idcli)
             ->where('idEstado',2)
             ->first();
         if($boleta){
             //Log::info($boleta);
-
-            $convenio = getenv('OTROS_PAGOS_COVENIO');
-            //Log::info($convenio);
-            $key = $request->p_fectr.$request->p_tid.$convenio;
-            $llave = str_pad($key,16);
-            $encriptacion = openssl_encrypt($llave, "AES-256-CBC",getenv('OTROS_PAGOS_KEY'),1,getenv('OTROS_PAGOS_IV'));
-            //Log::info($encriptacion);
-            $h_firma = base64_encode($encriptacion);
-            //Log::info($h_firma);
-            $headers = apache_request_headers();
-            foreach($headers as $header => $value){
-                if($header == "H-Firma"){
-                    if($value != $h_firma){
-                        return response()->json(['r_retcod' => "65"],200);
-                    }
-                }
-            }
             $idTransaccion = (int)$request->p_tid;
             if($idTransaccion){
             //Log::info($idTransaccion);
@@ -67,21 +66,24 @@ class OtrosPagosController extends Controller
 
     }
     public function notpag01req(Request $request){
-        $idTransaccion = (int)$request->p_tid;
-        if($idTransaccion){
-            $convenio = getenv('OTROS_PAGOS_COVENIO');
-            $key = $request->p_fectr.$request->p_tid.$convenio;
-            $llave = str_pad($key,16);
-            $encriptacion = openssl_encrypt($llave, "AES-256-CBC",getenv('OTROS_PAGOS_KEY'),1,getenv('OTROS_PAGOS_IV'));
-            $h_firma = base64_encode($encriptacion);
-            $headers = apache_request_headers();
-            foreach($headers as $header => $value){
-                if($header == "H-Firma"){
-                    if($value != $h_firma){
-                        return response()->json(['r_retcod' => "65"],200);
-                    }
+
+        $convenio = getenv('OTROS_PAGOS_COVENIO');
+        $key = $request->p_fectr.$request->p_tid.$convenio;
+        $llave = str_pad($key,16);
+        $encriptacion = openssl_encrypt($llave, "AES-256-CBC",getenv('OTROS_PAGOS_KEY'),1,getenv('OTROS_PAGOS_IV'));
+        $h_firma = base64_encode($encriptacion);
+        $headers = apache_request_headers();
+        foreach($headers as $header => $value){
+            if($header == "H-Firma"){
+                if($value != $h_firma){
+                    return response()->json(['r_retcod' => "65"],200);
                 }
             }
+        }
+
+        $idTransaccion = (int)$request->p_tid;
+        if($idTransaccion){
+            
             $boleta = Boleta::find($request->p_idcli);
             
             if($boleta){
@@ -128,20 +130,18 @@ class OtrosPagosController extends Controller
         $boleta = Boleta::find($request->p_idcli);
         $idTransaccion = (int)$request->p_tid;
         if($boleta){
-            if($boleta->idEstado == 3 || $boleta->idEstado == 2){
-                $boleta->idEstado = 5;
-                $boleta->save();
-                $numeros = Numero::where('idBoleta',$request->p_idcli)->get();
-                foreach($numeros as $numero){
-                    $numero->idBoleta = null;
-                    $numero->idEstado = 1;
-                    $numero->save();
-                }
-                return response()->json([
-                    'r_tid' => $idTransaccion,
-                    'r_retcod' => "00"
-                ],200);
+            $boleta->idEstado = 5;
+            $boleta->save();
+            $numeros = Numero::where('idBoleta',$request->p_idcli)->get();
+            foreach($numeros as $numero){
+                $numero->idBoleta = null;
+                $numero->idEstado = 1;
+                $numero->save();
             }
+            return response()->json([
+                'r_tid' => $idTransaccion,
+                'r_retcod' => "00"
+            ],200);
         }
         return response()->json([
             'r_tid' => $idTransaccion,
