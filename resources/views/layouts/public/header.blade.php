9e192c4eda199bc789201e0ed1476d59c165f6ad
Ivan <ivan.saez@informatica.isbast.com>
2021-02-08 14:03:10 -0300

    cambio style para letras dela pagina

--- resources/views/layouts/public/header.blade.php
+++ resources/views/layouts/public/header.blade.php
@@ -5,18 +5,18 @@
         <div class="toggle" onclick="toggleMenu()"></div>
         <ul class="navigation">
            <!-- <li><a href="propiedades.html">Propiedades</a></li> -->
-            <li><a href="{{ asset('tienda-rifo-propiedades') }}" style="color: white">Propiedades</a></li>
+            <li><a href="{{ asset('tienda-rifo-propiedades') }}" >Propiedades</a></li>
            <!-- <li><a href="bases-legales.html">Bases Legales</a></li> -->
             <div class="contNotificacion">
-                <li><a href="{{ asset('carrito-compra') }}" style="color: white">Carrito de Compra</a></li>
+                <li><a href="{{ asset('carrito-compra') }}" >Carrito de Compra</a></li>
                 @if (Session::has('carritoCompra'))
-                    <span class="notificacion" id="notificacion-span" style="color: white">{{ count(Session::get('carritoCompra')) }}</span>
+                    <span class="notificacion" id="notificacion-span" >{{ count(Session::get('carritoCompra')) }}</span>
                 @else
                     <span class="notificacion" id="notificacion-span" style="display: none"></span>
                 @endif
             </div>
 
-            <li><a href="{{ asset('/') }}#contacto" style="color: white">Contacto</a></li>
+            <li><a href="{{ asset('/') }}#contacto" >Contacto</a></li>
         </ul>
     </div>
 
